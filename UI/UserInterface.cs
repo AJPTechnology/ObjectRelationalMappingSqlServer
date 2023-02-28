10e748ac4a2f9649f633ddad24eee1cabb0cde99
António José Ferreira Pinto <124891685+AJPTechnology@users.noreply.github.com>
2023-02-28 16:13:37 +0000

    Update UserInterface.cs

--- UI/UserInterface.cs
+++ UI/UserInterface.cs
@@ -7,13 +7,13 @@ public class UserInterface
 	{
         clsPessoa user = new clsPessoa();
         List<clsPessoa> pessoa = user.GetAllTableData().Cast<clsPessoa>().ToList();
-        List<clsPessoa> pessoaByPk = user.GetTableDataByPk("1").Cast<clsUserLogin>().ToList();
+        var pessoaByPk = user.GetTableDataByPk("1").Cast<clsUserLogin>();
 
         clsPessoa p = new clsPessoa();
         p.Nome = "Nome1";
         p.Status = true;
         p.save();
 
-        pessoaByPk.delete();
+        user.delete();
     }
 }
